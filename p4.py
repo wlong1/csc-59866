4a00d211a745b6c764b6e0a5b5eb0bf1e7b54cea
wlong <109259315+wlong1@users.noreply.github.com>
2025-11-24 22:32:01 -0500

    Update p4.py

--- p4.py
+++ p4.py
@@ -11,14 +11,14 @@ df = pd.read_csv(filename)
 
 df = df[df['population'] > 1_000_000]
 
-coords = df[['lat', 'lng']].applymap(radians).to_numpy()
+coords = df[['lat', 'lng']].map(radians).to_numpy()
 dist_km = haversine_distances(coords) * 6371
 
-model = AgglomerativeClustering(n_clusters=10, linkage='average')
+model = AgglomerativeClustering(n_clusters=10, metric='precomputed', linkage='average')
 df['cluster'] = model.fit_predict(dist_km)
 
 plt.scatter(df['lng'], df['lat'], c=df['cluster'], cmap='tab10')
 plt.xlabel('Longitude')
 plt.ylabel('Latitude')
-plt.title('City Clusters')
+plt.title('Clusters')
 plt.show()
