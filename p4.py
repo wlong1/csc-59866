dbff8fd746a7d2ad5c5a0a5a4dee558bfbec1626
wlong <109259315+wlong1@users.noreply.github.com>
2025-11-24 22:27:47 -0500

    Added part 4

---
+++ p4.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics.pairwise import haversine_distances
+from sklearn.cluster import AgglomerativeClustering
+from math import radians
+
+
+filename = 'world_cities.csv'
+df = pd.read_csv(filename)
+
+df = df[df['population'] > 1_000_000]
+
+coords = df[['lat', 'lng']].applymap(radians).to_numpy()
+dist_km = haversine_distances(coords) * 6371
+
+model = AgglomerativeClustering(n_clusters=10, linkage='average')
+df['cluster'] = model.fit_predict(dist_km)
+
+plt.scatter(df['lng'], df['lat'], c=df['cluster'], cmap='tab10')
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
+plt.title('City Clusters')
+plt.show()
